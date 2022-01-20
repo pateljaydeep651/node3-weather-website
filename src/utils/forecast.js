526dd53f5b5e269e442e742f5e91f6735a96f2b2
Jaydeep Patel <pateljaydeep651@gmail.com>
2022-01-20 10:41:32 +0530

    Added humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) =>{
             callback('Unable to find location', undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees out')
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees out and humidity is ' + body.current.humidity)
         }
     })
 }
